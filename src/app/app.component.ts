c7b78f7cdc8c34607bedbb1519c0670081200324
demecarvq <demetrio.carvalho@cipalschaubroeck.be>
2018-01-23 17:10:27 +0100

    3 commit to trabis-ci branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { TrafficLightService, LIGHT_STATES } from './traffic-light.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  title = 'Traffic Signal App changed';
+  title = 'Traffic Signal App';
   trafficLightState: string;
 
   constructor(private trafficService: TrafficLightService) {
